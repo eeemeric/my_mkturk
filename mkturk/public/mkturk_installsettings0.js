768c245ae714150ea110d280ea4c28a4bda6b776
erik emeric <erik.emeric@gmail.com>
2025-12-15 18:28:41 -0500

    updated mkturk_instalationsettings0.js

--- mkturk/public/mkturk_installsettings0.js
+++ mkturk/public/mkturk_installsettings0.js
@@ -2,24 +2,24 @@
 // Also used for liveplot to identify where to look for behavioral files
 
 // ------ Dropbox WebApp settings ------ 
+// PATH & APP KEY from Dropbox Developer Console for
+// MKTURK.HTML & LIVEPLOT.HTML
 
-// MKTURK
-var DBX_CLIENT_ID = "2m9hmvxxxxxxxxx"
-var DBX_REDIRECT_URI_ROOT = "http://localhost:8080/" //local
-// var DBX_REDIRECT_URI_ROOT = "https://sandbox-ce2c5.firebaseapp.com/mkturk/" //firebase
+//var DBX_CLIENT_ID = "2m9hmv7q45kwren"
+//var DBX_REDIRECT_URI_ROOT = "https://dl.dropboxusercontent.com/spa/k79b8ph6lmcr30d/mkturk/public/"
+var DBX_CLIENT_ID = "f7att01psd18ckj"
+var DBX_REDIRECT_URI_ROOT = "https://eeemeric.github.io/my_mkturk/mkturk/public/"
+
 
 // ------ Subject settings ------ 
-var subjectlist = [
-"Eliaso","Michaelo"
-];
+// var subjectlist = ["ExampleSubject"];
+var subjectlist = ["Elasio"];
 
 // ------ Save location settings ------
-var DATA_SAVEPATH = "/mkturkfiles/datafiles/"
-var PARAM_DIRPATH = "/mkturkfiles/parameterfiles/subjects/"
-var SOUND_FILEPREFIX = "/mkturkfiles/sounds/au"
+var DATA_SAVEPATH = "/mkturkfolders/datafiles/"
+var PARAM_DIRPATH = "/mkturkfolders/parameterfiles/subjects/"
+var SOUND_FILEPREFIX = "/mkturkfolders/sounds/au"
 
 // ------ Misc. -----------------------
 var ndatafiles2read=5; // todo: change to trials. and use as upper bound (stop reading once you hit the first discrepancy). maybe this is effectively synonymous with mintrials
-var num_preload_images=5; // how long can you/the NHP bother waiting at each imageload? 400 images ~ 30 seconds. Recommended to keep = 0 with good internet connection and automator on
-
-// ------ todo: move into params file -
+var num_preload_images=0; // how long can you/the NHP bother waiting at each imageload? 400 images ~ 30 seconds. Recommended to keep = 0 with good internet connection and automator on
